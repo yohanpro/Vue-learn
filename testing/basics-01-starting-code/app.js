a3c354e33aa883da2023ead40fadff1611fc4b86
Yohan <yohan@yohanpro.com>
2021-01-03 16:44:02 +0900

    binding attributes

---
+++ testing/basics-01-starting-code/app.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            myTest: 'This is my Test',
+            myLink: 'https://yohanpro.com'
+        };
+    }
+});
+
+
+app.mount('#user-goal');
