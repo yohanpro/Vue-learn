2c407285d476c2c612d4018772838d12970a5bef
Yohan <yohan@yohanpro.com>
2021-01-08 12:04:27 +0900

    computed

--- testing/basics-03-events-starting-code/app.js
+++ testing/basics-03-events-starting-code/app.js
@@ -2,24 +2,39 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name:''
+      name: "",
     };
+  },
+  computed: {
+    fullname() {
+      console.log("running");
+      if (this.name === "") {
+        return "";
+      }
+      console.log("has");
+      return this.name + " " + "Kim";
+    },
   },
   methods: {
     setName(event, lastname) {
-      this.name = event.target.value+" "+lastname;
+      this.name = event.target.value + " " + lastname;
     },
     add() {
       this.counter = this.counter + 1;
     },
-    
+
     reduce() {
       this.counter = this.counter - 1;
     },
-    signUp() {
-      
-    }
-  }
+    signUp() {},
+    reset() {
+      this.name = "";
+    },
+    setName(event) {
+      console.log("this.name", this.name);
+      this.name = event.target.value;
+    },
+  },
 });
 
-app.mount('#events');
+app.mount("#events");
