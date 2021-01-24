df0376ee130a2bf9b556e184ba3958d687e56303
Yohan <yohan@yohanpro.com>
2021-01-24 20:41:04 +0900

    creating basic Vue app

---
+++ vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <h2 class="abcd">안녕하세요</h2>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      friends: [
+        {
+          name: "john",
+          email: "jon@example.com",
+        },
+      ],
+    };
+  },
+};
+</script>
