a9d41dc87ca0f34c8c343d529ee98e0ea3f4fb8d
Yohan <yohan@yohanpro.com>
2021-01-29 20:11:32 +0900

    added the dynamic props

---
+++ 08_component/src/components/FriendContact.vue
@@ -0,0 +1,50 @@
+<template>
+  <li>
+    <h2>{{ name }}</h2>
+    <button @click="toggleDetails">
+      {{ detailsAreVisible ? "Hide" : "Show" }} Details
+    </button>
+    <ul v-if="detailsAreVisible">
+      <li>
+        <strong>Phone:</strong>
+        {{ name }}
+      </li>
+      <li>
+        <strong>Email:</strong>
+        {{ email }}
+      </li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    email: {
+      default: '',
+      type: String
+    },
+    name: {
+      default: '',
+      type: String
+    },
+
+  },
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "manuel",
+        name: "Manuel Lorenz",
+        phone: "0123 45678 90",
+        email: "manuel@localhost.com",
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    }
+  }
+};
+</script>
