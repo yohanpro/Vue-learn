0377fcd0e6ed68f5004b63b33084a0b8fc1077e5
Yohan <yohan@yohanpro.com>
2021-02-12 20:09:36 +0900

    feat: Router 등록

--- finding-coach/src/router.js
+++ finding-coach/src/router.js
@@ -1,17 +1,28 @@
-import { createRouter, createWebHistroy } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
+
+import CoachDetail from './pages/coaches/CoachDetail.vue';
+import CoachesList from './pages/coaches/CoachesList.vue';
+import CoachRegistation from './pages/coaches/CoachRegistration.vue';
+import ContactCoach from './pages/requests/ContactCoach.vue';
+import RequestsReceived from './pages/requests/RequestsReceived.vue';
+import NotFound from './pages/NotFound.vue';
 
 const router = createRouter({
-  history: createWebHistroy(),
+  history: createWebHistory(),
   routes: [
-    { path: '/', redirectTo: '/coaches' },
-    { path: '/coaches', component: null },
+    { path: '/', redirect: '/coaches' },
+    { path: '/coaches', component: CoachesList },
     {
       path: '/coaches/:id',
-      component: null,
-      children: [{ path: 'contact', component: null }]
+      component: CoachDetail,
+      children: [
+        { path: 'contact', component: ContactCoach }, // /coaches/c1/contact
+      ],
     },
-    { path: '/register', component: null },
-    { path: '/requests', component: null },
-    { path: '/:notFound(.*)', component: null }
-  ]
+    { path: '/register', component: CoachRegistation },
+    { path: '/requests', component: RequestsReceived },
+    { path: '/:notFound(.*)', component: NotFound },
+  ],
 });
+
+export default router;
