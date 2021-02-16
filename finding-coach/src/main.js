a5c64f0ad3255e780f7c2c32419825bd3b975f63
Yohan <yohan@yohanpro.com>
2021-02-16 23:56:45 +0900

    Fix: Fixed the store configure in store/index.js

--- finding-coach/src/main.js
+++ finding-coach/src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 
 import router from './router.js'
 import App from './App.vue'
-import store from './store'
+import store from './store/index.js'
 
 const app = createApp(App)
 
--- finding-coach/src/store/index.js
+++ finding-coach/src/store/index.js
@@ -3,7 +3,9 @@ import { createStore } from 'vuex'
 import coachesModule from './modules/coaches/index.js'
 
 const store = createStore({
-  modules: coachesModule,
+  modules: {
+    coaches: coachesModule,
+  },
 })
 
 export default store
